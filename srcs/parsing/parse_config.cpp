da5ee7ebadeff6e589fd2c2fe4ed4f4ac6c9e683
Vincent Stineau <vstineau@bess-f2r1s6.clusters.42paris.fr>
2025-03-13 14:07:04 +0100

    loconfig

--- srcs/parsing/parse_config.cpp
+++ srcs/parsing/parse_config.cpp
@@ -225,6 +225,18 @@ static void	get_index(config &conf, std::string &buffer)
 	conf.server_index = buffer.substr(offset, pos - offset);
 }
 
+void get_loconfig(config & conf)
+{
+	conf.locations[""].root =  conf.root;
+	conf.locations[""].index_html =  conf.server_index;
+	conf.locations[""].client_body_size =  conf.client_body_size;
+	conf.locations[""].directory_listing =  conf.directory_listing;
+	conf.locations[""].allowed_method[0] =  conf.allowed_method[0];
+	conf.locations[""].allowed_method[1] =  conf.allowed_method[1];
+	conf.locations[""].allowed_method[2] =  conf.allowed_method[2];
+	conf.locations[""].allowed_method[3] =  conf.allowed_method[3];
+}
+
 void	get_one_config(config &conf, std::string &buffer)
 {
 	get_server_name(conf, buffer);
@@ -234,8 +246,9 @@ void	get_one_config(config &conf, std::string &buffer)
 	get_server_port(conf, buffer);
 	is_directory_listing_allowed(conf, buffer);
 	set_upload_directory(conf, buffer);
-	get_locations_bloc(conf, buffer);
 	get_method_allowed(conf, buffer);
+	get_locations_bloc(conf, buffer);
+	get_loconfig(conf);
 }
 
 //location = /
